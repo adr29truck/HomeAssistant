8011237471d66ebb994f8f6369217f23b212167d
adr29truck <adrianalmetunsmeds@gmail.com>
2018-10-07 14:06:42 +0200

    Added support for lovelace player
    Added support for lovelace player by @thomasloven
    https://github.com/thomasloven/lovelace-player

---
+++ www/lovelace-player-config.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+
+  // Add your bindings here
+  // Example:
+  // LovelacePlayer.bind("media_player.main_pc", "9684c2f2-41754ec1");
+
+  // Remove this line when you found your Device ID
+ // LovelacePlayer.debug();
+}, 200);
