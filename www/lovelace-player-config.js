613bdd65b2b819f7ad8d0dfff2c568f9d6476698
adr29truck <adrianalmetunsmeds@gmail.com>
2018-10-07 15:09:09 +0200

    Added a second media player
    This still don´t work. But it´s improving

--- www/lovelace-player-config.js
+++ www/lovelace-player-config.js
@@ -2,8 +2,9 @@ setTimeout(function() {
 
   // Add your bindings here
   // Example:
-  // LovelacePlayer.bind("media_player.main_pc", "9684c2f2-41754ec1");
+   LovelacePlayer.bind("media_player.main_pc", "9684c2f2-41754ec1");
+   LovelacePlayer.bind("media_player.honor", "7f1c7920-ac9123cb");
 
   // Remove this line when you found your Device ID
- // LovelacePlayer.debug();
+  //LovelacePlayer.debug();
 }, 200);
