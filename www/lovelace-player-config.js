88612c89002179fcf85f35fc65685a3f0299d837
adr29truck <adrianalmetunsmeds@gmail.com>
2018-11-24 21:33:36 +0100

    Updated a still broken lovelace-player and made voicerss the default tts engine + added florrplan configuration

--- www/lovelace-player-config.js
+++ www/lovelace-player-config.js
@@ -2,9 +2,11 @@ setTimeout(function() {
 
   // Add your bindings here
   // Example:
-   LovelacePlayer.bind("media_player.main_pc", "9684c2f2-41754ec1");
-   LovelacePlayer.bind("media_player.honor", "7f1c7920-ac9123cb");
-
+  //  LovelacePlayer.bind("media_player.main_pc", "9684c2f2-41754ec1");
+  //  LovelacePlayer.bind("media_player.honor", "7f1c7920-ac9123cb");
+  LovelacePlayer.bind("media_player.main_pc_again", "ef73f6b2-b0df9aee");
+  LovelacePlayer.bind("media_player.android", "3a49589-9d24cf92");
+  
   // Remove this line when you found your Device ID
-  //LovelacePlayer.debug();
+  // LovelacePlayer.debug();
 }, 200);
